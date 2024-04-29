c17377d630a67cae51d780136604b55477be9874
AviatorCoderr <124370316+AviatorCoderr@users.noreply.github.com>
2024-04-29 23:22:41 +0530

    connection error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:8000'
+      '/api': 'https://pearlcrest1-o5me79i7v-kushagra-sahays-projects.vercel.app'
     }
   },
   plugins: [react()],
