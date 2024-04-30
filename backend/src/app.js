9597036b67b42e3e40a945b5d2ec1681d1418966
AviatorCoderr <124370316+AviatorCoderr@users.noreply.github.com>
2024-04-30 19:02:41 +0530

    deply error

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,9 +6,10 @@ const app = express()
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"]
 }))
-
+    
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
