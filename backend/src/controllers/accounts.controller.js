174ce73348c72c0e0c589a497caadef0e8bc3793
AviatorCoderr <124370316+AviatorCoderr@users.noreply.github.com>
2024-05-02 08:58:26 +0530

    error

--- backend/src/controllers/accounts.controller.js
+++ backend/src/controllers/accounts.controller.js
@@ -287,7 +287,7 @@ const getCashBalance = asyncHandler(async (req, res) => {
 const getTransaction5 = asyncHandler(async(req, res) => {
   const {flatnumber} = req.body
   console.log(flatnumber)
-  const flat = Flat.findOne({flatnumber})
+  const flat = await Flat.findOne({flatnumber})
   console.log(flat)
   const data = await Transaction.find({ flat: flat._id }).limit(5);
   console.log(data);
