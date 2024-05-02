89991f471f3086ee1d26295f443ee988557c7fc8
AviatorCoderr <124370316+AviatorCoderr@users.noreply.github.com>
2024-05-02 09:22:08 +0530

    error debug

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -5,16 +5,21 @@ import { asyncHandler } from "../utils/asynchandler.js"
 
 export const verifyJWT = asyncHandler(async(req, res, next) =>{
     try {
+        console.log("hello i am inside error page")
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer", "")
+        console.log("token recieved")
         if(!token){
             throw new ApiError(401, "Unauthorised request")
         }
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        console.log("jwt verified")
         const flat = await Flat.findById(decodedToken?._id).select("-password -refreshToken")
+        console.log("flat receieved")
         if(!flat){
             throw new ApiError(401, "Invalid Access Token")
         }
         req.flat = flat
+        console.log("flat assigned")
         next()
     } catch (error) {
         throw new ApiError(401, error?.message || "Invalid Access Token")
