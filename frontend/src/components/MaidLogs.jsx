85cb34423ab0385ca31cfdcb300d1965afab9786
AviatorCoderr <124370316+AviatorCoderr@users.noreply.github.com>
2024-05-16 19:33:06 +0530

    error

--- frontend/src/components/MaidLogs.jsx
+++ frontend/src/components/MaidLogs.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Maid from '../../../backend/src/models/maids.model';
 
 export default function MaidLog() {
     const [maid, setMaid] = useState([]);
