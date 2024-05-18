b3d151144c42c568fe7d1d996498e74bb82d5320
AviatorCoderr <124370316+AviatorCoderr@users.noreply.github.com>
2024-05-18 09:18:19 +0530

    fixed again

--- frontend/src/components/RegistrationPage.jsx
+++ frontend/src/components/RegistrationPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import RegisterFlat from "./components/RegisterFlat"
-import RegisterOwner from "./components/RegisterOwner"
+import RegisterFlat from "./RegisterFlat"
+import RegisterOwner from "./RegisterOwner"
 const RegistrationPage = () => {
     return (
         <div className="grid grid-cols-2 gap-8">
