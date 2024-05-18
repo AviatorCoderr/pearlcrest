1ef087f171185067b3fde8ab23c9a1697899bcd7
AviatorCoderr <124370316+AviatorCoderr@users.noreply.github.com>
2024-05-18 14:07:12 +0530

    pics uploaded

--- frontend/src/components/MemberCouncil.jsx
+++ frontend/src/components/MemberCouncil.jsx
@@ -23,14 +23,13 @@ const MemberCouncil = () => {
     { id: 2, position: 'SECRETARY', name: 'Mr. Sunil Kumar', flat: 'B403', contact: '7909058947', url: '/static/images/the secretary.jpg' },
     { id: 3, position: 'TREASURER', name: 'Mr. Manish', flat: 'A104', contact: '9431177995', url: '/static/images/treasurer.jpg' },
     { id: 4, position: 'Executive Member', name: 'Mr. Shailendra Kumar', flat: 'A301', contact: '8340258087', url: '/static/images/executive_member.jpg' },
-    { id: 5, position: 'Executive Member', name: 'Mrs. Monalisa Singh', flat: 'A405', contact: '7667768225', url: '/static/images/executive_member.jpg' },
-    { id: 6, position: 'Executive Member', name: 'Mr. Manoj Kumar Singh', flat: 'B204', contact: '9431328432', url: '/static/images/executive_member.jpg' },
-    { id: 7, position: 'Executive Member', name: 'Mr. Sunil Kumar Singh', flat: 'CG3', contact: '9781077819', url: '/static/images/executive_member.jpg' },
-    { id: 8, position: 'Executive Member', name: 'Mrs. Indu Prasad', flat: 'CG1', contact: '9431924288', url: '/static/images/executive_member.jpg' },
-    { id: 9, position: 'Executive Member', name: 'Mrs. Poonam Nischal', flat: 'C208', contact: '8809360519', url: '/static/images/executive_member.jpg' },
-    { id: 10, position: 'Executive Member', name: 'Mr. Lalit Kishore', flat: 'C306', contact: '8877327018', url: '/static/images/executive_member.jpg' },
-    { id: 11, position: 'Executive Member', name: 'Mr. Amit Kumar', flat: 'C311', contact: '8709931191', url: '/static/images/executive_member.jpg' },
-    { id: 12, position: 'Executive Member', name: 'Mr. Ranjan Kumar', flat: 'D-block', contact: '9334902514', url: '/static/images/executive_member.jpg' }
+    { id: 5, position: 'Executive Member', name: 'Mrs. Monalisa Singh', flat: 'A405', contact: '7667768225', url: '/static/images/monalisa-min.jpg' },
+    { id: 6, position: 'Executive Member', name: 'Mr. Manoj Kumar Singh', flat: 'B204', contact: '9431328432', url: '/static/images/manoj-min.jpg' },
+    { id: 7, position: 'Executive Member', name: 'Mr. Sunil Kumar Singh', flat: 'CG3', contact: '9781077819', url: '/static/images/sunil-min.jpg' },
+    { id: 8, position: 'Executive Member', name: 'Mrs. Indu Prasad', flat: 'CG1', contact: '9431924288', url: '/static/images/indu-min.jpg' },
+    { id: 9, position: 'Executive Member', name: 'Mrs. Poonam Nischal', flat: 'C208', contact: '8809360519', url: '/static/images/poonam-min.jpg' },
+    { id: 10, position: 'Executive Member', name: 'Mr. Lalit Kishore', flat: 'C306', contact: '8877327018', url: '/static/images/lalit-min.jpg' },
+    { id: 11, position: 'Executive Member', name: 'Mr. Ranjan Kumar', flat: 'D-block', contact: '9334902514', url: '/static/images/ranjan-min.jpg' }
   ];
 
   return (
