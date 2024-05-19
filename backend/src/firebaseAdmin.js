dd60eb3dc9c15ee22d302e2d88a7657f91415bb8
AviatorCoderr <124370316+AviatorCoderr@users.noreply.github.com>
2024-05-19 13:34:32 +0530

    notification

--- backend/src/firebaseAdmin.js
+++ backend/src/firebaseAdmin.js
@@ -1,11 +1,25 @@
 // firebaseAdmin.js
 
 import admin from 'firebase-admin';
-import serviceAccount from './serviceAccountKey.json' assert { type: 'json' };
+import dotenv from 'dotenv';
+
+dotenv.config(); // Load environment variables from .env file
 
 // Initialize Firebase Admin SDK with service account credentials
 const app = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    type: process.env.GOOGLE_CLOUD_TYPE,
+    project_id: process.env.GOOGLE_CLOUD_PROJECT_ID,
+    private_key_id: process.env.GOOGLE_CLOUD_PRIVATE_KEY_ID,
+    private_key: process.env.GOOGLE_CLOUD_PRIVATE_KEY.replace(/\\n/g, '\n'), // Replace escaped newlines
+    client_email: process.env.GOOGLE_CLOUD_CLIENT_EMAIL,
+    client_id: process.env.GOOGLE_CLOUD_CLIENT_ID,
+    auth_uri: process.env.GOOGLE_CLOUD_AUTH_URI,
+    token_uri: process.env.GOOGLE_CLOUD_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.GOOGLE_CLOUD_AUTH_PROVIDER_CERT_URL,
+    client_x509_cert_url: process.env.GOOGLE_CLOUD_CLIENT_CERT_URL,
+    universe_domain: process.env.GOOGLE_CLOUD_UNIVERSE_DOMAIN
+  }),
   // Add additional options if needed
 });
 
